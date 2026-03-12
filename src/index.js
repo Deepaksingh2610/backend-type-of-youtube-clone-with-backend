63acd7c7f2bff6ff028745ad86ef067338f7b53b
Deepaksingh2610 <ds0854252@gmail.com>
2026-03-12 12:35:20 +0530

    understand api errors and response and add some code in utils

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ dotenv.config({
 
 
 connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 9000,()=>{
+        console.log(`server is running at port :${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!!", err)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+
+// we can used both promises and try catach
+const asyncHandler = (requestHandler) => {
+    (req,res, next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+// try and catch method ways
+// const asyncHandler = (fn) => async (req, res, next) =>{
+//   try {
+//     await fn(req,res,next)
+    
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//         success: false,
+//         message:error.message
+//     })
+//   }
+// }
