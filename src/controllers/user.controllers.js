96334e754b97a9c46c70a4c0a0d7a5273bd0e2e2
Deepaksingh2610 <ds0854252@gmail.com>
2026-03-21 17:44:03 +0530

    image check user abel to register using postman

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -29,7 +29,7 @@ const registerUser =  asyncHandler(async (req, res)=>{
   }
 
   //check user alredy exists or not
- const existedUser=  User.findOne({
+ const existedUser= await User.findOne({
     $or:[{username},{email}]
   })
   if (existedUser){
@@ -38,7 +38,12 @@ const registerUser =  asyncHandler(async (req, res)=>{
 
   //check images and avatar
 const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath= req.files?.coverImage[0]?.path;
+// const coverImageLocalPath= req.files?.coverImage[0]?.path;
+
+let coverImageLocalPath;
+if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+   coverImageLocalPath = req.files.coverImage[0].path
+}
 if (!avatarLocalPath){
     throw new ApiError(400, " Avatar file is required")
 } 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,19 +50,18 @@ const userSchema = new Schema({
     timestamps:true
 })
 
-userSchema.pre("save",async function (next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    if(!this.isModified("password")) return;
 
-    this.password = await bcrypt.hash(this.password, 8)
-    next()
+    this.password = await bcrypt.hash(this.password, 8);
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password)
-},
+  return await bcrypt.compare(password, this.password);
+}
 
-userSchema.methods.generateAccessToken= function (){
-    jwt.sign({
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign({
         _id: this._id,
         email: this.email,
         username: this.username,
@@ -75,7 +74,7 @@ userSchema.methods.generateAccessToken= function (){
  )
 }
 userSchema.methods.generateRefreshToken = function(){
-     jwt.sign({
+     return jwt.sign({
         _id: this._id,
         
     },
