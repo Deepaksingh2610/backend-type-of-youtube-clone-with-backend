1fe976dabc2e1053689e8b2245794366c99f02f0
Deepaksingh2610 <ds0854252@gmail.com>
2026-03-25 10:59:28 +0530

    fixing file name and spelling while importing there was error

--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import { createPlaylist, getUserPlaylists } from "../controllers/playlist.controllers.js";
 
 
 const playlistSchema = new Schema(
---
+++ test_imports.js
@@ -0,0 +1,5 @@
+import { createPlaylist } from "./src/controllers/playlist.controllers.js";
+import { getChannelStats } from "./src/controllers/dashboard.controllers.js";
+import { createTweet } from "./src/controllers/tweet.controllers.js";
+
+console.log("Imports successful!");
