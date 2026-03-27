c5daed69d50a9ca65331887419e7f54f2052e6eb
Singh deepak jitendra <157622772+Deepaksingh2610@users.noreply.github.com>
2026-03-28 00:02:03 +0530

    Refactor CORS configuration with allowed origins

--- src/app.js
+++ src/app.js
@@ -4,10 +4,26 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://backend-type-of-youtube-clone-with-imbb.onrender.com"
+];
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
