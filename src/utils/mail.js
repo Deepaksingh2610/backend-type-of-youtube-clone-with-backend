eb158cebf2d8399dbe521f4b8f87223e892db2b0
Singh deepak jitendra <157622772+Deepaksingh2610@users.noreply.github.com>
2026-03-28 09:17:49 +0530

    Update mail.js

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -2,9 +2,7 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false,
+        service: "gmail",
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
@@ -12,7 +10,7 @@ const sendEmail = async (options) => {
     });
 
     const mailOptions = {
-        from: process.env.SMTP_FROM,
+        from: process.env.SMTP_USER,
         to: options.email,
         subject: options.subject,
         text: options.message,
@@ -23,3 +21,50 @@ const sendEmail = async (options) => {
 };
 
 export default sendEmail;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import nodemailer from "nodemailer";
+
+// const sendEmail = async (options) => {
+//     const transporter = nodemailer.createTransport({
+//         host: process.env.SMTP_HOST,
+//         port: process.env.SMTP_PORT,
+//         secure: false,
+//         auth: {
+//             user: process.env.SMTP_USER,
+//             pass: process.env.SMTP_PASS,
+//         },
+//     });
+
+//     const mailOptions = {
+//         from: process.env.SMTP_FROM,
+//         to: options.email,
+//         subject: options.subject,
+//         text: options.message,
+//         html: options.html,
+//     };
+
+//     await transporter.sendMail(mailOptions);
+// };
+
+// export default sendEmail;
